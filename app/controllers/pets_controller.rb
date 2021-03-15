85c8300a9d3310148be2dba06ff67d35762a79f5
Taylor Treece <taylor.treece@protonmail.com>
2021-03-15 14:43:25 -0500

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -11,9 +11,8 @@ class PetsController < ApplicationController
   end
 
   post '/pets' do 
-    binding.pry
 
-    @pet=Pet.create(params[pet])
+    @pet=Pet.create(params['pet'])
     redirect to "pets/#{@pet.id}"
   end
 
